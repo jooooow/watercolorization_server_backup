14eb95b28d6b7a9758568fd4c339eae2c80cbfb0
jooooow <hjm64kou@gmail.com>
2022-12-22 18:24:00 +0900

    20221222_iphone

--- server.py
+++ server.py
@@ -34,15 +34,16 @@ def upload():
 
 @socketio.on('connect', namespace=name_space)
 def connected_msg():
-    print('client connected.')
-    event_name = 'dcenter'
     sid = request.sid
+    print(f'client connected to {sid}')
+    event_name = 'dcenter'
     data = {'sid': sid}
     socketio.emit(event_name, data, room=sid, namespace=name_space)
 
 @socketio.on('disconnect', namespace=name_space)
 def disconnect_msg():
-    print('client disconnected.')
+    sid = request.sid
+    print(f'client disconnected from {sid}')
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=1234)
